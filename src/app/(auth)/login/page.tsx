6a416076b54e8d2f90d61b411ce28deee2bd2617
Piotrek <piotrek@ps-design.pl>
2024-07-08 15:27:33 +0200

    register and login pages work in progress

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,19 @@
+import { FormInput } from "@/components/shared/form/FormInput";
+import { signup } from "../actions";
+
 export default function Login() {
-    return <h2>Login</h2>
+  return (
+    <div className="container mx-auto p-12 flex flex-col items-center justify-between">
+      <h2 className="text-2xl font-semibold text-center mb-4">Log in</h2>
+      <form className="border-1 border-regal-blue-light/20 rounded-lg p-4 flex flex-col justify-between content-start">
+        <FormInput id="email" label="Email" placeholder="" />
+        <FormInput id="password" label="Password" placeholder="" />
+        <div className="p-2 w-full">
+          <button formAction={signup} className="w-full bg-regal-red text-regal-beige py-2 px-4 rounded-with-transition-on-hover cursor-pointer hover:bg-regal-red-light">
+            Log in
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,3 +1,20 @@
+import { FormInput } from "@/components/shared/form/FormInput";
+import { signup } from "../actions";
+
 export default function Register() {
-    return <h2 className="text-c">Register</h2>
+  return (
+    <div className="container mx-auto p-12 flex flex-col items-center justify-between">
+      <h2 className="text-2xl font-semibold text-center mb-4">Create new user</h2>
+      <form className="border-1 border-regal-blue-light/20 rounded-lg p-4 flex flex-col justify-between content-start">
+        <FormInput id="email" label="Email" placeholder="" />
+        <FormInput id="password" label="Password" placeholder="" />
+        <FormInput id="repeat-password" label="Repeat password" placeholder="" />
+        <div className="p-2 w-full">
+          <button formAction={signup} className="w-full bg-regal-red text-regal-beige py-2 px-4 rounded-with-transition-on-hover cursor-pointer hover:bg-regal-red-light">
+            Register now
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
